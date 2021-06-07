914a535429c81bc52096bcce6e83225336fd7289
Yellowbeard-26 <Siddh.nigam26@gmail.com>
2021-06-07 19:27:33 +0530

    News App

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -1,14 +1,77 @@
 package com.example.newsapp;
 
+import android.os.Bundle;
+import android.widget.GridView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.newsapp.adapters.Gridcategoryadapter;
+import com.glide.slider.library.SliderLayout;
+import com.glide.slider.library.slidertypes.BaseSliderView;
+import com.glide.slider.library.slidertypes.DefaultSliderView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    SliderLayout sliderLayout;
+    GridView gridView;
+    Gridcategoryadapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        InitiateViews();
+
+        addimagestoslider();
+    }
+
+    private void addimagestoslider() {
+
+        List<Integer> images=new ArrayList<>();
+        images.add(R.drawable.img1);
+        images.add(R.drawable.img2);
+        images.add(R.drawable.img3);
+        images.add(R.drawable.img4);
+
+        for(int i=0;i<images.size();i++)
+        {
+            DefaultSliderView defaultSliderView=new DefaultSliderView(this);
+            defaultSliderView.setRequestOption(new RequestOptions().centerCrop());
+            defaultSliderView.image(images.get(i));
+            defaultSliderView.setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
+                @Override
+                public void onSliderClick(BaseSliderView slider) {
+
+                }
+            });
+        sliderLayout.addSlider(defaultSliderView);
+        }
+        sliderLayout.startAutoCycle();
+        sliderLayout.setPresetTransformer(SliderLayout.Transformer.Stack);
+        sliderLayout.setDuration(3000);
+        sliderLayout.setPresetIndicator(SliderLayout.PresetIndicators.Right_Bottom);
+
+    }
+
+    private void InitiateViews() {
+        sliderLayout=findViewById(R.id.slider);
+        gridView=findViewById(R.id.grid_view);
+        adapter=new Gridcategoryadapter(this);
+        gridView.setAdapter(adapter);
+
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        sliderLayout.stopAutoCycle();
     }
 }
---
+++ app/src/main/java/com/example/newsapp/adapters/Gridcategoryadapter.java
@@ -0,0 +1,88 @@
+package com.example.newsapp.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.newsapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class Gridcategoryadapter extends BaseAdapter {
+
+    LayoutInflater layoutInflater;
+    Context context;
+    List<DemoCategory> demoCategories;
+
+    public Gridcategoryadapter(Context context) {
+        this.context = context;
+        layoutInflater=(LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        demoCategories=new ArrayList<>();
+
+        demoCategories.add(new DemoCategory("Basics", R.drawable.img1));
+        demoCategories.add(new DemoCategory("Basic", R.drawable.img4));
+        demoCategories.add(new DemoCategory("Basics", R.drawable.img1));
+        demoCategories.add(new DemoCategory("Basics", R.drawable.img1));
+        demoCategories.add(new DemoCategory("Basics", R.drawable.img1));
+        demoCategories.add(new DemoCategory("Basics", R.drawable.img1));
+        demoCategories.add(new DemoCategory("Basics", R.drawable.img1));
+        demoCategories.add(new DemoCategory("Basics", R.drawable.img1));
+
+    }
+
+    @Override
+    public int getCount() {
+        return demoCategories.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return demoCategories.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHoldery holdery=null;
+        if(convertView==null)
+        {
+            convertView=layoutInflater.inflate(R.layout.item_category_layout,null);
+            holdery=new ViewHoldery();
+            holdery.circleImageView=convertView.findViewById(R.id.category_image);
+            holdery.textView=convertView.findViewById(R.id.text_category);
+            convertView.setTag(holdery);
+        }
+        else
+        {
+            holdery=(ViewHoldery) convertView.getTag();
+        }
+        holdery.textView.setText(demoCategories.get(position).Imagename);
+        Glide.with(context)
+                .load(demoCategories.get(position).imageid).into(holdery.circleImageView);
+        return convertView;
+    }
+    static class ViewHoldery{
+        CircleImageView circleImageView;
+        TextView textView;
+    }
+    class DemoCategory{
+        String Imagename;
+        int imageid;
+
+        public DemoCategory(String imagename, int imageid) {
+            Imagename = imagename;
+            this.imageid = imageid;
+        }
+    }
+}
